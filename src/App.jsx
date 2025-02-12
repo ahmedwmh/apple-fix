587f30c4ac453fd50b43594ea3eb61f13b32376f
AHMED MH <46631725+Ahmed95Mohammed@users.noreply.github.com>
2025-02-12 21:44:50 +0300

    Apple iphone 15

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+import * as Sentry from '@sentry/react';
+
+const App = () => {
+  return (
+    <main className="bg-black">
+        <h1 className='pt-10 px-10 bg-red-500'>HELLO WORLD</h1>
+    </main>
+  )
+}
+
+export default App;
